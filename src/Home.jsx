a8dbcafb8217ae020af4bb85e33e742133d5d7a6
Brian4755 <brian.mccune1@yahoo.com>
2024-01-12 10:05:36 -0600

    home info

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,10 @@
 const Home = () => {
   return ( 
     <div>
-      This is the home section
+      <div className="introTitle">Hi, I'm Brian</div>
+      <div className="introTitle">Web Developer</div>
+      <div className="introSubTitle">Front End Developer / ReactJS Focused / THREEJS Fan </div>
+      <div className="introBio">Self-challenging and detail-oriented front-end developer, keeping up with emerging trends and technologies within web development.</div>
     </div>
    );
 }
