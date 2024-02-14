9991f3976bb6a84e8095f1d79dfdd2d7b32331be
Brian4755 <brian.mccune1@yahoo.com>
2024-02-14 01:50:25 -0600

    base navbar positioning and setup

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,9 +2,18 @@ import CodeIcon from "./CodeIcon";
 
 const Navbar = () => {
   return ( 
-    <>
-      <CodeIcon />
-    </>
+    <div className="navbar">
+      <div className="navbar-header">
+        <CodeIcon />
+        <h1>D V L P R</h1>
+      </div>
+      <div className="navbar-sections">
+        <h2>About</h2>
+        <h2>Projects</h2>
+        <h2>Skills</h2>
+        <h2>Contact</h2>
+      </div>
+    </div>
    );
 }
  
