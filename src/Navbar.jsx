093a85939e3e4ab2b6f459522129edb932e9085c
Brian4755 <brian.mccune1@yahoo.com>
2024-02-16 01:59:33 -0600

    button styling and color theme changes

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = () => {
     <div className="navbar">
       <div className="navbar-header">
         <CodeIcon />
-        <h1>D V L P R</h1>
+        <h1>McCune</h1>
       </div>
       <div className="navbar-sections">
         <h2>About</h2>
         <h2>Projects</h2>
         <h2>Skills</h2>
-        <h2>Contact</h2>
+        <button>Let's Talk</button>
       </div>
     </div>
    );
