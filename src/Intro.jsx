823cff6cabcdbb777b9d13271f368a9fcee50da1
Brian4755 <brian.mccune1@yahoo.com>
2024-02-16 09:45:38 -0600

    gsap installation and setup

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,10 +1,17 @@
-const Intro = () => {
+import gsap from "gsap";
+import { useGSAP } from '@gsap/react';
 
+const Intro = () => {
 
   const introOne = "HI THERE, I'M"
   const introTwo = "BRIAN MCCUNE."
   let intOneArray = introOne.split('')
   let intTwoArray = introTwo.split('')
+
+  useGSAP(() => {
+    // gsap.from('.name', {duration: 0.4, y:30, delay: 0.5})
+
+  },{})
 
   return ( 
     <div className="intro">
