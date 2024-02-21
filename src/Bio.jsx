d9ee1bd004b776b2fc03740f75f24e3147ca5912
Brian4755 <brian.mccune1@yahoo.com>
2024-02-21 09:40:10 -0600

    gsap scroll trigger base setup

--- src/Bio.jsx
+++ src/Bio.jsx
@@ -1,7 +1,28 @@
+import gsap from "gsap";
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 const Bio = () => {
+
+  useGSAP(() => {
+    gsap.to('.bio', {
+      scrollTrigger: {
+        trigger: '.bio',
+        start: 'top center'
+      },
+      // color: black,
+      x: 50,
+      duration: 5
+    })
+  })
+
+
   return ( 
     <div className="bio">
-      I create webpages, keeping up with emerging trends and technologies within web development.
+      <h2>
+        I create webpages, keeping up with emerging trends and technologies within web development.
+      </h2>
     </div>
    );
 }
