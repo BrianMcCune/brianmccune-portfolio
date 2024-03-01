2e638604045ca1468570f8967e11ea77f74dcadd
Brian4755 <brian.mccune1@yahoo.com>
2024-03-01 03:59:37 -0600

    smooth animation fix for projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -31,7 +31,7 @@ const Projects = () => {
         start: '-20% top',
         end: 'bottom center',
         toggleActions: "restart reverse restart reverse",
-        markers: true
+        // markers: true
       },
       backgroundColor: 'rgb(9, 9, 9)',
       color: 'rgb(241, 241, 241)',
