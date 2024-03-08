2a4f38c8620cb76a0c92688311488b72effcf769
Brian4755 <brian.mccune1@yahoo.com>
2024-03-08 03:15:52 -0600

    contact base positioning

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -20,21 +20,23 @@ const Contact = () => {
             </a>
           </div>
         </div>
-        <div className="contact-projects">
-          <div>Projects</div>
-          <p>Take a look at some projects I've created, showcasing my skills.</p>
-        </div>
-        <div className="contact-expertise">
-          <div>Expertise</div>
-          <p>Check out the areas of front-end development I am great at.</p>
-        </div>
-        <div className="contact-about">
-          <div>About</div>
-          <p>Here's a small bio about myself and what I do.</p>
-        </div>
-        <div className="contact-resume">
-          <div>Resume</div>
-          <p>See my work history and my achievements from my resume.</p>
+        <div className='summary-container'>
+          <div className="contact-about">
+            <div>About</div>
+            <p>Here's a small bio about myself and what I do.</p>
+          </div>
+          <div className="contact-expertise">
+            <div>Expertise</div>
+            <p>Check out the areas of front-end development I am great at.</p>
+          </div>
+          <div className="contact-projects">
+            <div>Projects</div>
+            <p>Take a look at some projects I've created, showcasing my skills.</p>
+          </div>
+          <div className="contact-resume">
+            <div>Resume</div>
+            <p>See my work history and my achievements from my resume.</p>
+          </div>
         </div>
       </div>
       <div className="copyright">© 2024 Brian McCune</div>
