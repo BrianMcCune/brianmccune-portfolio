d23e9aa8ef214994fd3be2f98d7134a143c22d05
Brian4755 <brian.mccune1@yahoo.com>
2024-03-16 04:23:01 -0500

    navbar about animation hover

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,10 +4,30 @@ import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 import CodeIcon from "./CodeIcon";
+import { useRef } from "react";
 
 
 const Navbar = (props) => {
 
+  const aboutRef = useRef(null)
+
+
+  const createHoverEffect = (element) => {
+    gsap.to(element.current, { 
+      width: 40,
+      duration: 0.2
+    });
+  }
+
+  const removeHoverEffect = (element) => {
+    gsap.to(element.current, { 
+      x: 41,
+      duration: 0.2,
+      onComplete: () => {
+        gsap.set(element.current, { x: 0, width: 0 });
+      }
+    })
+  }
 
   useGSAP(() => {
     gsap.from('.navbar', {
@@ -23,7 +43,12 @@ const Navbar = (props) => {
         <CodeIcon />
         <Link to="intro" className="link"><h1>McCune</h1></Link>
       </div>
-      <Link to="about" offset={90} className="link"><h2>About</h2></Link>
+      <Link to="about" offset={90} className="link">
+        <h2 className="navbar-about" onMouseEnter={() => {createHoverEffect(aboutRef)}} onMouseLeave={() => removeHoverEffect(aboutRef)}>
+          About
+          <div className="underline" ref={aboutRef}></div>
+        </h2>
+      </Link>
       <Link to="expertise" offset={-70}className="link"><h2>Expertise</h2></Link>
       <Link to="projects" offset={115} className="link"><h2>Projects</h2></Link>
       <Link to="contact" className="link">
