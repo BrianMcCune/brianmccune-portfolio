9ce08a8ba951770aa61d1193f18f922ce9c19ced
Brian4755 <brian.mccune1@yahoo.com>
2024-11-11 23:13:12 -0600

    testing

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -30,70 +30,70 @@ const Projects = () => {
     // ScrollTrigger.refresh()
   })
 
-  const createHoverEffect = (element, index) => {
-    mm.add("(min-width: 768px)", () => {
-      const backgroundColors = ['rgb(200, 221, 233)', 'rgb(235, 194, 96)', 'rgb(120, 171, 219)', 'rgb(248, 191, 95)']
+  // const createHoverEffect = (element, index) => {
+  //   mm.add("(min-width: 768px)", () => {
+  //     const backgroundColors = ['rgb(200, 221, 233)', 'rgb(235, 194, 96)', 'rgb(120, 171, 219)', 'rgb(248, 191, 95)']
 
-      gsap.to(element, { 
-        scale: 1.05, 
-        duration: 0.2, 
-        ease: 'none',
-        background: backgroundColors[index]
-      });
+  //     gsap.to(element, { 
+  //       scale: 1.05, 
+  //       duration: 0.2, 
+  //       ease: 'none',
+  //       background: backgroundColors[index]
+  //     });
       
-      gsap.to(`.project-image-container:nth-child(${index + 1}) > a > .project-image`, { 
-        scale: 0.95, 
-        duration: 0.2, 
-        ease: 'none',
-      });
+  //     gsap.to(`.project-image-container:nth-child(${index + 1}) > a > .project-image`, { 
+  //       scale: 0.95, 
+  //       duration: 0.2, 
+  //       ease: 'none',
+  //     });
 
-      gsap.to(`.project-image-container:nth-child(${index + 1}) > a > .project-tech`, {
-        opacity: 1,
-        duration: 0.2,
-      })
-    })
-  };
+  //     gsap.to(`.project-image-container:nth-child(${index + 1}) > a > .project-tech`, {
+  //       opacity: 1,
+  //       duration: 0.2,
+  //     })
+  //   })
+  // };
 
-  const removeHoverEffect = (element) => {
-    mm.add("(min-width: 768px)", () => {
-      gsap.to(element, { 
-        scale: 0.95, 
-        duration: 0.2,
-        ease: 'none', 
-        background: 'rgb(241, 241, 241)'
-      });
+  // const removeHoverEffect = (element) => {
+  //   mm.add("(min-width: 768px)", () => {
+  //     gsap.to(element, { 
+  //       scale: 0.95, 
+  //       duration: 0.2,
+  //       ease: 'none', 
+  //       background: 'rgb(241, 241, 241)'
+  //     });
 
-      gsap.to(`.project-image`, { 
-        scale: 1.05, 
-        duration: 0.2,
-        ease: 'none', 
-      });
+  //     gsap.to(`.project-image`, { 
+  //       scale: 1.05, 
+  //       duration: 0.2,
+  //       ease: 'none', 
+  //     });
 
-      gsap.to('.project-tech', {
-        opacity: 0,
-        duration: 0.2,
-      })
-    })
-  };
+  //     gsap.to('.project-tech', {
+  //       opacity: 0,
+  //       duration: 0.2,
+  //     })
+  //   })
+  // };
 
-  const headline = 'Projects'
-  let headlineArray = headline.split('')
+  // const headline = 'Projects'
+  // let headlineArray = headline.split('')
 
   useGSAP(() => {
-    gsap.from('.projects-headline', {
-      scrollTrigger: {
-        trigger: '.projects',
-        start: 'top center',
-        toggleActions: 'play none none none',
-      },
-      duration: 0.4, 
-      y:250, 
-      stagger: 0.05, 
-      ease: 'power3.out',
-      delay: 0.1
-    })
+    // gsap.from('.projects-headline', {
+    //   scrollTrigger: {
+    //     trigger: '.projects',
+    //     start: 'top center',
+    //     toggleActions: 'play none none none',
+    //   },
+    //   duration: 0.4, 
+    //   y:250, 
+    //   stagger: 0.05, 
+    //   ease: 'power3.out',
+    //   delay: 0.1
+    // })
 
-    // if (counter === 1) {
+    if (counter === 1) {
       // ScrollTrigger.refresh()
       tl = gsap.timeline({
         scrollTrigger: {
@@ -105,19 +105,19 @@ const Projects = () => {
         }
       })
       // ScrollTrigger.refresh()
-    // } else {
+    } else {
       // ScrollTrigger.refresh()
-      // tl = gsap.timeline({
-      //   scrollTrigger: {
-      //     trigger: '.projects',
-      //     start: 'top center',
-      //     end: 'bottom top',
-      //     scrub: true,
-      //     stagger: 0
-      //   }
-      // })
+      tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.projects',
+          start: 'top center',
+          end: 'bottom top',
+          scrub: true,
+          stagger: 0
+        }
+      })
       // ScrollTrigger.refresh()
-    // }
+    }
 
     tl.to('.projects', {
       backgroundColor: 'rgb(9, 9, 9)',
@@ -200,7 +200,7 @@ const Projects = () => {
 
   return ( 
     <div className="projects" id="projects">
-      <div className="headline-container">
+      {/* <div className="headline-container">
         {headlineArray.map((index) => {
           return (
             <div className="projects-headline" key={index}>
@@ -210,15 +210,16 @@ const Projects = () => {
             </div>
           )
         })}
-      </div>
+      </div> */}
       <div className="projects-list">
         {projectData.map((project, index) => {
           return (
             <div className="project-image-container" 
               key={index}
               ref={(el) => {elementRefs.current[index] = el}}
-              onMouseEnter={() => {createHoverEffect(elementRefs.current[index], index)}}
-              onMouseLeave={() => removeHoverEffect(elementRefs.current[index])}>
+              // onMouseEnter={() => {createHoverEffect(elementRefs.current[index], index)}}
+              // onMouseLeave={() => removeHoverEffect(elementRefs.current[index])}
+              >
               <a href={project.deployment} target="_blank">
                 <div className="project-name">{project.title}</div>
                 <div className="project-tech">{project.usedTech}</div>
