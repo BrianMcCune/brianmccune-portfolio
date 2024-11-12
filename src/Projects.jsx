82fff6402f7eda15237f36721c912da95e389049
Brian4755 <brian.mccune1@yahoo.com>
2024-11-11 22:49:38 -0600

    counter change

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,4 +1,4 @@
-import { useRef, useLayoutEffect } from "react";
+import { useRef, useLayoutEffect, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -6,78 +6,40 @@ gsap.registerPlugin(ScrollTrigger);
 import { projectData } from "./project-data";
 
 const Projects = () => {
-
-  // ScrollTrigger.refresh()
-
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
   const elementRefs = useRef([]);
-  let mm = gsap.matchMedia();
-  let counter
-  let tl
+  let tl;
 
-  mm.add("(min-width: 768px)", () => {
-    if (counter === 2) {
-      window.location.reload()
-    }
-    counter = 1
-    // ScrollTrigger.refresh()
-  })
+  useLayoutEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
 
-  mm.add("(max-width: 767px)", () => {
-    if (counter === 1) {
-      window.location.reload()
-    }
-    counter = 2
-    // ScrollTrigger.refresh()
-  })
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const createHoverEffect = (element, index) => {
-    mm.add("(min-width: 768px)", () => {
-      const backgroundColors = ['rgb(200, 221, 233)', 'rgb(235, 194, 96)', 'rgb(120, 171, 219)', 'rgb(248, 191, 95)']
-
-      gsap.to(element, { 
-        scale: 1.05, 
-        duration: 0.2, 
-        ease: 'none',
-        background: backgroundColors[index]
-      });
-      
-      gsap.to(`.project-image-container:nth-child(${index + 1}) > a > .project-image`, { 
-        scale: 0.95, 
-        duration: 0.2, 
-        ease: 'none',
-      });
-
-      gsap.to(`.project-image-container:nth-child(${index + 1}) > a > .project-tech`, {
-        opacity: 1,
-        duration: 0.2,
-      })
-    })
+    if (isDesktop) {
+      const backgroundColors = ['rgb(200, 221, 233)', 'rgb(235, 194, 96)', 'rgb(120, 171, 219)', 'rgb(248, 191, 95)'];
+      gsap.to(element, { scale: 1.05, duration: 0.2, ease: 'none', background: backgroundColors[index] });
+      gsap.to(`.project-image-container:nth-child(${index + 1}) > a > .project-image`, { scale: 0.95, duration: 0.2, ease: 'none' });
+      gsap.to(`.project-image-container:nth-child(${index + 1}) > a > .project-tech`, { opacity: 1, duration: 0.2 });
+    }
   };
 
   const removeHoverEffect = (element) => {
-    mm.add("(min-width: 768px)", () => {
-      gsap.to(element, { 
-        scale: 0.95, 
-        duration: 0.2,
-        ease: 'none', 
-        background: 'rgb(241, 241, 241)'
-      });
-
-      gsap.to(`.project-image`, { 
-        scale: 1.05, 
-        duration: 0.2,
-        ease: 'none', 
-      });
-
-      gsap.to('.project-tech', {
-        opacity: 0,
-        duration: 0.2,
-      })
-    })
+    if (isDesktop) {
+      gsap.to(element, { scale: 0.95, duration: 0.2, ease: 'none', background: 'rgb(241, 241, 241)' });
+      gsap.to(`.project-image`, { scale: 1.05, duration: 0.2, ease: 'none' });
+      gsap.to('.project-tech', { opacity: 0, duration: 0.2 });
+    }
   };
 
-  const headline = 'Projects'
-  let headlineArray = headline.split('')
+  const headline = 'Projects';
+  let headlineArray = headline.split('');
 
   useGSAP(() => {
     gsap.from('.projects-headline', {
@@ -87,101 +49,50 @@ const Projects = () => {
         toggleActions: 'play none none none',
       },
       duration: 0.4, 
-      y:250, 
+      y: 250, 
       stagger: 0.05, 
       ease: 'power3.out',
-      delay: 0.1
-    })
+      delay: 0.1,
+    });
 
-    if (counter === 1) {
-      // ScrollTrigger.refresh()
+    if (isDesktop) {
       tl = gsap.timeline({
         scrollTrigger: {
           trigger: '.projects',
           start: 'center bottom',
           end: '+=2250px',
           scrub: true,
-          stagger: 0
+          stagger: 0,
+          markers: true,
         }
-      })
-      // ScrollTrigger.refresh()
+      });
     } else {
-      // ScrollTrigger.refresh()
       tl = gsap.timeline({
         scrollTrigger: {
           trigger: '.projects',
           start: 'top center',
           end: 'bottom top',
           scrub: true,
-          stagger: 0
+          stagger: 0,
         }
-      })
-      // ScrollTrigger.refresh()
+      });
     }
 
-    tl.to('.projects', {
-      backgroundColor: 'rgb(9, 9, 9)',
-      duration: 0.1,
-      stagger: 0
-    }, 0)
+    tl.to('.projects', { backgroundColor: 'rgb(9, 9, 9)', duration: 0.1 }, 0);
+    tl.to('.expertise, .contact, .navbar', { backgroundColor: 'rgb(9, 9, 9)', color: 'rgb(241, 241, 241)', duration: 0.1 }, 0);
+    tl.to('h2, h1', { color: 'rgb(241, 241, 241)', duration: 0.1 }, 0);
+    tl.to('button, .underline', { backgroundColor: 'rgb(241, 241, 241)', color: 'rgb(9, 9, 9)' }, 0);
 
-    tl.to('.expertise, .contact, .navbar', {
-      backgroundColor: 'rgb(9, 9, 9)',
-      color: 'rgb(241, 241, 241)',
-      duration: 0.1,
-    }, 0)
+    tl.to('.projects-headline, .contact-headline, .fontawesomeicon', { color: 'rgb(241, 241, 241)' }, 0);
+    tl.to('.navbar, .expertise, .contact, body', { backgroundColor: 'rgb(9, 9, 9)', color: 'rgb(241, 241, 241)', duration: 0.8 });
+    tl.to('.projects', { color: 'rgb(9, 9, 9)', duration: 0.8 }, 0.2);
+    tl.to('.projects, .navbar, .expertise, .contact, body', { color: 'rgb(9, 9, 9)', backgroundColor: 'rgb(241, 241, 241)', duration: 0.1 });
+    tl.to('h2, h1', { color: 'rgb(9, 9, 9)', duration: 0.1 });
+    tl.to('button, .underline', { backgroundColor: 'rgb(9, 9, 9)', color: 'rgb(241, 241, 241)' }, 1);
+    tl.to('.projects-headline, .contact-headline', { color: 'rgb(110, 110, 85)' }, 1);
+    tl.to('.fontawesomeicon', { color: 'rgb(9, 9, 9)' }, 1);
 
-    tl.to('h2, h1', {
-      color: 'rgb(241, 241, 241)',
-      duration: 0.1,
-    }, 0)
-
-    tl.to('button, .underline', {
-      backgroundColor: 'rgb(241, 241, 241)',
-      color: 'rgb(9, 9, 9)',
-    }, 0)
-
-    tl.to('.projects-headline, .contact-headline, .fontawesomeicon', {
-      color: 'rgb(241, 241, 241)'
-    }, 0)
-
-    tl.to('.navbar, .expertise, .contact, body', {
-      backgroundColor: 'rgb(9, 9, 9)',
-      color: 'rgb(241, 241, 241)',
-      duration: 0.8
-    })
-
-    tl.to('.projects', {
-      color: 'rgb(9, 9, 9)',
-      duration: 0.8
-    }, 0.2)
-
-    tl.to('.projects, .navbar, .expertise, .contact, body', {
-      color: 'rgb(9, 9, 9)',
-      backgroundColor: 'rgb(241, 241, 241)',
-      duration: 0.1
-    })
-
-    tl.to('h2, h1', {
-      color: 'rgb(9, 9, 9)',
-      duration: 0.1,
-    })
-
-    tl.to('button, .underline', {
-      backgroundColor: 'rgb(9, 9, 9)',
-      color: 'rgb(241, 241, 241)',
-    }, 1)
-
-    tl.to('.projects-headline, .contact-headline', {
-      color: 'rgb(110, 110, 85)'
-    }, 1)
-
-    tl.to('.fontawesomeicon', {
-      color: 'rgb(9, 9, 9)'
-    }, 1)
-
-    mm.add("(min-width: 768px)", () => {
-
+    if (isDesktop) {
       gsap.to('.project-image-container', {
         xPercent: -310,
         ease: 'none',
@@ -192,41 +103,34 @@ const Projects = () => {
           pin: true,
           scrub: 1,
           anticipatePin: 1,
-          // markers: true
         },
-      })
-    })
-  })
+      });
+    }
+  });
 
-  return ( 
+  return (
     <div className="projects" id="projects">
       <div className="headline-container">
-        {headlineArray.map((index) => {
-          return (
-            <div className="projects-headline" key={index}>
-              <span className="letter">
-                {index === ' ' ? '\u00A0' : index}
-              </span>
-            </div>
-          )
-        })}
+        {headlineArray.map((index) => (
+          <div className="projects-headline" key={index}>
+            <span className="letter">{index === ' ' ? '\u00A0' : index}</span>
+          </div>
+        ))}
       </div>
       <div className="projects-list">
-        {projectData.map((project, index) => {
-          return (
-            <div className="project-image-container" 
-              key={index}
-              ref={(el) => {elementRefs.current[index] = el}}
-              onMouseEnter={() => {createHoverEffect(elementRefs.current[index], index)}}
-              onMouseLeave={() => removeHoverEffect(elementRefs.current[index])}>
-              <a href={project.deployment} target="_blank">
-                <div className="project-name">{project.title}</div>
-                <div className="project-tech">{project.usedTech}</div>
-                <img className="project-image" src={project.image} />
-              </a>
-            </div>
-          )
-        })}
+        {projectData.map((project, index) => (
+          <div className="project-image-container"
+            key={index}
+            ref={(el) => {elementRefs.current[index] = el}}
+            onMouseEnter={() => {createHoverEffect(elementRefs.current[index], index)}}
+            onMouseLeave={() => removeHoverEffect(elementRefs.current[index])}>
+            <a href={project.deployment} target="_blank">
+              <div className="project-name">{project.title}</div>
+              <div className="project-tech">{project.usedTech}</div>
+              <img className="project-image" src={project.image} />
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
